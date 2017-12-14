39bf7c2105f7765d4d82538d8557200fc0f5e227
Daniel Kummer <daniel.kummer@gmail.com>
2017-12-14 21:09:38 +0100

    Update python library

--- onion2-python-library/ni2c/ni2c/__init__.py
+++ onion2-python-library/ni2c/ni2c/__init__.py
@@ -1,56 +1,60 @@
+import logging
+import re
 
 
 class Ni2c:
     MAX_MODULES = 8
     NI2C_OK = 0
-    CHAR_MAPPING = {'n': 0, '+': 1, 'm': 2, 'A': 3, 'V': 4, 'M': 5, 'o': 6, 'i': 7, 'K': 8, '-': 9, ' ': 0xF}
+
+    CHAR_MAPPING = 'n+mAVMoiK-'
 
     def __init__(self, i2c, number_of_modules=1, start_address=0x38):
         self.debug = False
         self.number_of_modules = number_of_modules
         self.start_address = start_address
-        self.data_out = 0xFF
         self.error = self.NI2C_OK
         self.i2c = i2c
 
+    def write_2_module(self, module, tupel):
+        address = self.start_address + module
+
+        out_value = 0xff
+
+        for idx, character in enumerate(tupel):
+            value = self._get_i2c_value(character)
+            out_value = (out_value & 0xF0) | value if idx % 2 == 0 else (out_value & 0x0F) | (value << 4)
+
+        return self.i2c.writeByte(address, 0x00, out_value)
+
     def write(self, string):
         num_of_chars = len(string)
-        if num_of_chars > self.number_of_modules * 2:
-            raise ValueError('The string length exceeds the number of configured modules')
+        is_equal = num_of_chars % 2 == 0
 
-        result = 0xff
+        result = self.NI2C_OK
 
-        for idx, c in enumerate(string):
-            module = self._get_module_for_char_position(idx)
-            address = self._get_i2c_address(module)
+        if not is_equal:
+            string += ' '
+            logging.debug('Appended extra character to create string of equal length')
 
-            value = self._get_i2c_value(c)
-
-            nixie_index = self._get_nixie_index(idx, num_of_chars)
-
-            result = self._write_i2c(address, value, nixie_index)
-            if result > 0:
-                self.error = result
-                break
+        for module, tupel in enumerate(re.findall('..', string)):
+            if module < self.number_of_modules:
+                result = self.write_2_module(module, tupel)
+                if result > 0:
+                    self.error = result
+                    break
+            else:
+                logging.debug('Exceeded max number of allowed characters based on number of modules')
+                return result
 
         return result
 
-    def _get_nixie_index(self, character_index, num_of_characters):
-        nixie_index = character_index % 2
-        if character_index == num_of_characters - 1 and nixie_index == 1:
-            nixie_index = 0xFF
-        return nixie_index
-
     def _get_i2c_value(self, character):
-        if character.isdigit():
-            value = int(character)
-        else:
-            value = self.CHAR_MAPPING[character]
-        return value
+        value = int(character) if character.isdigit() else self.CHAR_MAPPING.find(character)
+        return value if value >= 0 else 0xF
 
     def off(self):
-        for i in xrange(0, self.MAX_MODULES + 1):
-            self._write_i2c(self._get_i2c_address(i), 0xFF, 2)
+        for i in xrange(0, self.number_of_modules):
+            self.write_2_module(i, "  ")
 
     def last_error(self):
         e = self.error
@@ -67,27 +71,3 @@ class Ni2c:
 
         self.error = self.NI2C_OK
         return e
-
-    def _get_i2c_address(self, module_number):
-        return self.start_address + ((module_number - 1) * 2)
-
-    def _get_module_for_char_position(self, char_position):
-        in_min = 0.0
-        in_max = self.MAX_MODULES * 2.0
-        out_min = 1.0
-        out_max = self.MAX_MODULES * 1.0
-        return int(round((char_position - in_min) * (out_max - out_min) / (in_max - in_min) + out_min))
-
-    def _write_i2c(self, address, value, nixie_index):
-        if nixie_index == 0:
-            self.data_out = self.data_out & 0x0F | (value << 4)
-        elif nixie_index == 1:
-            self.data_out = self.data_out & 0xF0 | value
-        else:
-            self.data_out = value
-
-        result = 0x00
-        if nixie_index > 0:
-            result = self.i2c.writeByte(address, 0x00, value)
-
-        return result
--- onion2-python-library/ni2c/ni2c/tests/test_ni2c.py
+++ onion2-python-library/ni2c/ni2c/tests/test_ni2c.py
@@ -22,36 +22,14 @@ class TestNi2c(TestCase):
         self.i2c_start_address = 0x38
         self.nixie = ni2c.Ni2c(MockOnionI2C(), 1, self.i2c_start_address)
 
-    def test_write_should_throw_exception_when_string_exeeds_modules(self):
-        with self.assertRaises(ValueError):
-            self.nixie.write("1234")
-
-    def test_get_i2c_address_should_return_correct_address(self):
-        for i in range(1, 9):
-            self.assertEqual(self.nixie._get_i2c_address(i), self.i2c_start_address + ((i-1) * 2))
-
-    def test_get_module_for_char_position_should_return_module_number_in_pairs_of_two(self):
-        self.assertEqual(self.nixie._get_module_for_char_position(0), 1)
-        self.assertEqual(self.nixie._get_module_for_char_position(1), 1)
-        self.assertEqual(self.nixie._get_module_for_char_position(2), 2)
-        self.assertEqual(self.nixie._get_module_for_char_position(3), 2)
-
-    def test_write(self):
-        result = self.nixie.write("12")
+    def test_write_should_succeed(self):
+        result = self.nixie.write(" 1")
         self.assertEqual(result, 0x00)
 
     def test_get_i2c_value(self):
         for i in range(0, 10):
             self.assertEqual(self.nixie._get_i2c_value("%d" % (i)), i)
-        for idx, i in enumerate(['n', '+','m','A','V','M','o','i','K','-']):
+        for idx, i in enumerate(['n', '+', 'm', 'A', 'V', 'M', 'o', 'i', 'K', '-']):
             self.assertEqual(self.nixie._get_i2c_value(i), idx)
 
         self.assertEqual(self.nixie._get_i2c_value(" "), 0xf)
-
-    def test_get_nixie_index(self):
-        self.assertEqual(self.nixie._get_nixie_index(0, 2), 0)
-        self.assertEqual(self.nixie._get_nixie_index(1, 2), 0xff)
-        self.assertEqual(self.nixie._get_nixie_index(2, 4), 0)
-        self.assertEqual(self.nixie._get_nixie_index(3, 4), 0xff)
-        self.assertEqual(self.nixie._get_nixie_index(3, 5), 1)
-        self.assertEqual(self.nixie._get_nixie_index(4, 5), 0)
